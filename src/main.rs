a4c2a56721e3d3c3d94423647292f2766563c17e
Joshua Gross <joshua.gross@gmail.com>
2020-11-25 11:08:39 -0800

    Parse "programs": semicolon-separated expr list

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod parser;
 
-use parser::Expr;
+use parser::{Expr};
 
 fn main() {
   println!("foo")
@@ -8,14 +8,19 @@ fn main() {
 
 #[test]
 fn parse_expr_test() {
+  // Parse basic numbers and var references
   assert_eq!(parser::parse_expr("1.1"), Ok(("", Expr::Float(1.1))));
   assert_eq!(parser::parse_expr("2.7"), Ok(("", Expr::Float(2.7))));
   assert_eq!(parser::parse_expr("hello"), Ok(("", Expr::Var("hello".to_string()))));
   assert_eq!(parser::parse_expr("foobar"), Ok(("", Expr::Var("foobar".to_string()))));
 
-  // TODO: figure out how to test errors
-  //assert_eq!(parse_expr("1five"), Err(nom::Err::Error("five", nom::Err::Eof)));
-  assert_eq!(parser::parse_expr("1five"), Err(nom::Err::Error(nom::error::Error::new(&"five"[..], nom::error::ErrorKind::Eof))));
-  assert_eq!(parser::parse_expr("five 1"), Err(nom::Err::Error(nom::error::Error::new(&" 1"[..], nom::error::ErrorKind::Eof))));
+  // Parse a basic "Program"
+  assert_eq!(parser::parse_program("foobar;1.3"), Ok(("", vec![Expr::Var("foobar".to_string()), Expr::Float(1.3)])));
+
+  // Errors with basic parsing
+  assert_eq!(parser::parse_program("1five"), Err(nom::Err::Error(nom::error::Error::new(&"five"[..], nom::error::ErrorKind::Eof))));
+  assert_eq!(parser::parse_program("five 1"), Err(nom::Err::Error(nom::error::Error::new(&" 1"[..], nom::error::ErrorKind::Eof))));
+
+  // Parse multiple statements
 }
 
