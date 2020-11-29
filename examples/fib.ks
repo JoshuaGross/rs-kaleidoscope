fa8b10043f301162f2aba3364504ac37504c63ee
Joshua Gross <joshua.gross@gmail.com>
2020-11-29 00:59:43 -0800

    cleanup

--- examples/fib.ks
+++ examples/fib.ks
@@ -1,3 +1,5 @@
+extern printd(char);
+
 # Compute the x'th fibonacci number.
 def fib(x)
   if x < 3 then
@@ -6,4 +8,4 @@ def fib(x)
     fib(x-1)+fib(x-2);
 
 # This expression will compute the 40th number.
-fib(40)
+printd(fib(40));
--- src/codegen.rs
+++ src/codegen.rs
@@ -48,7 +48,6 @@ static EXTERNAL_FNS: [extern fn(f64) -> f64; 2] = [putchard, printd];
 ///
 /// Calling this is innately `unsafe` because there's no guarantee it doesn't
 /// do `unsafe` operations internally.
-//type SumFunc = unsafe extern "C" fn(u64, u64, u64) -> u64;
 type MainFunc = unsafe extern "C" fn() -> f64;
 
 pub struct CodeGen<'a, 'ctx> {
@@ -62,45 +61,6 @@ pub struct CodeGen<'a, 'ctx> {
     fn_value_opt: Option<FunctionValue<'ctx>>
 }
 impl<'a, 'ctx> CodeGen<'a, 'ctx> {
-    /*pub fn jit_compile_sum(&self) -> Option<JitFunction<SumFunc>> {
-        let i64_type = self.context.i64_type();
-        let fn_type = i64_type.fn_type(&[i64_type.into(), i64_type.into(), i64_type.into()], false);
-        let function = self.module.add_function("sum", fn_type, None);
-        let basic_block = self.context.append_basic_block(function, "entry");
-
-        self.builder.position_at_end(basic_block);
-
-        let x = function.get_nth_param(0)?.into_int_value();
-        let y = function.get_nth_param(1)?.into_int_value();
-        let z = function.get_nth_param(2)?.into_int_value();
-
-        let sum = self.builder.build_int_add(x, y, "sum");
-        let sum = self.builder.build_int_add(sum, z, "sum");
-
-        self.builder.build_return(Some(&sum));
-
-        // Verify the function
-        if function.verify(true) {
-          // Print function before optimization
-          println!("Function before optimization:");
-          function.print_to_stderr();
-
-          // Optimize the function
-          self.fpm.run_on(&function);
-
-          // Print function after optimization
-          println!("Function after optimization:");
-          function.print_to_stderr();
-
-          // JIT compile the function
-          unsafe { self.execution_engine.get_function("sum").ok() }
-        } else {
-          unsafe { function.delete(); }
-
-          None
-        }
-    }*/
-
     // Gets a defined function given its name.
     // Directly from https://github.com/TheDan64/inkwell/blob/master/examples/kaleidoscope/main.rs
     #[inline]
